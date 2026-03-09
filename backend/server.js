e5ea49708996f770db849a15fc753eed84106cd3
unknown <abhijeetkasar97@gmail.com>
2026-03-09 19:16:08 +0530

    update api url

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const { v4: uuidv4 } = require("uuid");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors({
   origin: "https://blostemassignment1.vercel.app"
